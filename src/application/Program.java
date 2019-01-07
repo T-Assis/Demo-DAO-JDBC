8816ac8eaa947b66c8a3a0ec9dcde9f0ce3a48c9
Thales Lima de Assis <thaleslima-19@hotmail.com>
2019-01-07 12:28:05 -0200

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,18 @@
 package application;
 
-import java.util.Date;
-import java.util.List;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Department department = new Department(1, "Books");
-		System.out.println(department);
 		
-		Seller seller = new Seller(3, "thales", "thales@gmail.com", new Date(), 5000.00, department);
+		// Test findById implementation
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
